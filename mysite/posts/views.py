763ace6bf0fb6166c618a1e04929f936845c0ed7
jaylenw <mr2cleanj@live.com>
2014-05-24 20:15:57 -0700

    edited views.py for the better, I hope

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from posts.models import Post #Post class in models.py
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the posts index.")
+    latest_post_list = Post.objects.order_by('-pub_date')[:5]
+    context = {'latest_post_list':latest_post_list}
+    return render(request, 'posts/index.html', context)
+    #visitor sees five latest posts
 
 def detail(request, post_id):
-    return HttpResponse("You are looking at post %s." % post_id)
+    try:
+      post = Post.objects.get(pk=post_id)
+    except Post.DoesNotExist:
+      raise Http404
+    return render(request, 'posts/detail.html',{'post':post})
     #look at comments in views.py
+    #visitor visits specific post
