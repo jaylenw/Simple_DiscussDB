3b191405fad591dbdf4b4385b6883e306bd8ea2d
jaylenw <mr2cleanj@live.com>
2014-05-24 19:10:23 -0700

    wrote more views, regex, functions

--- mysite/posts/urls.py
+++ mysite/posts/urls.py
@@ -3,5 +3,10 @@ from django.conf.urls import patterns, url
 from posts import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    #ex: /posts/
+    url(r'^$', views.index, name='index'),
+    #ex: /posts/5/
+    url(r'^(?P<post_id>\d+)/$', views.detail, name='detail'),
+    #the above url is using regex expressions talking to
+    #def details in views.py
 )
--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the posts index.")
+
+def detail(request, post_id):
+    return HttpResponse("You are looking at post %s." % post_id)
+    #look at comments in views.py
