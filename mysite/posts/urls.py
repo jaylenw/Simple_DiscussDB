0d9039259918048eb3ce4c7b46257bed4f8c2970
jaylenw <mr2cleanj@live.com>
2014-10-16 13:07:21 -0700

    added crucial comments for url use

--- mysite/posts/urls.py
+++ mysite/posts/urls.py
@@ -6,7 +6,15 @@ urlpatterns = patterns('',
     #ex: /posts/
     #ex: /posts/5/
     url(r'^(?P<post_id>\d+)/$', views.detail, name='detail'),
+
+    #so in the url of browser, to acess specific post, root/posts/x/, where
+    #x is the number (id) refering to the post
+
+
     #the above url is using regex expressions talking to
     #def detail in views.py
     url(r'^feeder/', views.paginator, name="paginator"),
+
+    #so in the url of browser, access
+    #the feed by root/posts/feeder
 )
